2a7a1053f2637787a18535159abffdad210e8c08
Roman Kremlin <roman@Romans-MacBook-Air.local>
2025-12-17 11:51:11 +0300

    remove about link for deploy

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -20,9 +20,6 @@
 			<li aria-current={page.url.pathname === '/' ? 'page' : undefined}>
 				<a href={resolve('/')}>Home</a>
 			</li>
-			<li aria-current={page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href={resolve('/about')}>About</a>
-			</li>
 			<li aria-current={page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
 				<a href={resolve('/sverdle')}>Sverdle</a>
 			</li>
